6ee32b8797c8bd44037396da3bfe280d1c2f0fc0
Leandro Arns <leandro.arns@gmail.com>
2020-05-20 14:51:24 -0300

    criando links de 'esqueci minha senha' e 'criar conta' na tela de autenticaçao

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu Logon!</Title>
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+    <>
+      <Container>
+        <Image source={logoImg} />
+        <Title>Faça seu Logon!</Title>
 
-      <Button onPress={() => { }} >Entrar</Button>
-    </Container>
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+
+        <Button onPress={() => { }} >
+          Entrar
+        </Button>
+
+        <ForgotPassword onPress={() => { }}>
+
+          <ForgotPasswordText>Esqueci Minha Senha</ForgotPasswordText>
+
+        </ForgotPassword>
+      </Container >
+
+      <CreateAccountButton onPress={() => { }} >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>
+          Criar Conta
+        </CreateAccountButtonText>
+      </CreateAccountButton>
+
+    </>
   );
 };
 
